02859af164031a45da2ef1b4a28196817fdb6db7
Will Potter <will@wpotter.me>
2013-04-22 12:39:35 -0400

    inital commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+#Javascript Data Structures
+
+I decided to create this library to try and implement data structures in Javascript to provide a standard interface for using many abstract data types.
+
+
+##Supported Now
+* Vector
+
+
+##To be supported
+* SinglyLinkedList
+* DoublyLinkedList
+* Tree
+* Graph
---
+++ structure.js
@@ -0,0 +1,37 @@
+/*
+Javascript Data Structures
+structure.js
+
+Author: Will Potter
+
+
+*/
+
+// Vector Implementation
+var Vector = function() {
+	this.data = [];
+}
+Vector.prototype = {
+	get: function(i) {
+		return this.data[i];
+	},
+	set: function(i, val) {
+		this.data[i] = val;
+	},
+	remove: function(i) {
+		val = this.data[i];
+		for (var j = i; j < this.data.length - 1; j++) {
+			this.data[j] = this.data.[j + 1];
+		}
+		this.data[this.data.length - 1] = undefined;
+	},
+	size: function() {
+		return this.data.length;
+	}
+}
+
+var Stack = function() {
+	this.data = []
+	this.last = 0;
+}
+Stack.
